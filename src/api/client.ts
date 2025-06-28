8e69a94a0d1f8251d6d2239a65e28a76e3bcd94d
jabinenterprises <testdude254@gmail.com>
2025-06-28 11:40:50 +0300

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001/api";
+const API_BASE_URL = "https://treeserver.vercel.app/api";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
